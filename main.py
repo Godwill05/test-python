56e204ebb5c4575aa0b8480fd2bcf79ae1e6e0a0
Godwill05 <godwilloussou@gmail.com>
2025-05-01 12:28:56 +0100

    erreur engendé

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello word'
+# main.py
+def saluer(nom):
+    return f"Bonjour, {nom} !"
+
+if __name__ == "__main__":
+    utilisateur = "Alice"
+    message = saluer(utilisateur)
+    print(message)
+
